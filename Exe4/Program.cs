0effd178a6bd1f32ff26e76bcc6a702830e40249
MohammadHossein Labafbashi <92016155+Labafbashi@users.noreply.github.com>
2022-11-18 00:49:18 +0100

    Finish Exercise 2

--- Exe4/Program.cs
+++ Exe4/Program.cs
@@ -36,13 +36,13 @@ internal class Program
     {
         /*
          * Theory and facts
-         * 1. Stack like a qeueu and it's LIFO (Last Input First Output) data structure, that means access to data only from the top part. 
-         * Each datatype that work as stack, automatically remove from memory. for example:
+         * 1. Stack is like a queue and it's LIFO (Last Input First Output) data structure, which means access to data only from the top part. Each datatype that works as a stack automatically removes from memory. for example:
          * STACK => [bool type][integer][double][][][][][][][]
          * Heap => [      string         classProgram            Object   ]
          * 
-         * 2. Value type always hold a value that declar inandvanced for example int num=5, it's means num is integer number and hold value 5.
-         * If call the num it's return 5 until change the value. But, Reference type refer/point to other place of memory or object. For example
+         * Heap is a place in the memory, any place in memory can sit and we can refer to this type of variable with a memory address. This type of variable needs a Garbage collector to remove it from memory.
+         * 
+         * 2. Value type always holds a value that declaration in advance for example int num=5, which means num is an integer number and holds the value 5. If we call the num it returns 5 until it changes the value. But, Reference type refers/points to another place of memory or object. For example:
          * Car car = new Car("BENZ") 
          */
         int num = 5;
@@ -50,8 +50,7 @@ internal class Program
         Object obj = new object();
         Console.WriteLine("Reference Type obj: " + obj.GetType());
         /*
-         * 3. The First method using a Value Type and it's Stack, so the variables x and y has a exact integer number.
-         * But in the second method we are using an object and refer to a place of Heap (Reference Type).
+         * 3. The first method uses a Value Type and its Stack, so the variables x and y have an exact integer number. But in the second method, we are using an object and refer to a place of Heap (Reference Type).
          * 
          * Debug of Return Value                        Debug of Return Value 2
          * line     x     y    return                   line     x      x.myValue   y       y.myValue       return
@@ -66,7 +65,45 @@ internal class Program
          */
         Console.WriteLine("Return Value (First Method): " + new MyInt().ReturnValue());
         Console.WriteLine("Return Value 2 (Second Method): " + new MyInt().ReturnValue2());
-        
+
+        /* Exercise1: ExamineList()
+         * 2. When a list definition, we can define a list with elements or without elements. Then in the code, with Add method, we can add an element to the list. 
+         * 3. As a default when we add an element to the list at first, it automatically creates binary series (4,8,16,32,...) blocks of memory space  when they are full then it adds more new spaces again.
+         * 4. It helps to speed up memory allocation, if one by one adding a new space of memory it needs a longer time to work till is created one time and used.
+         * 5. When we remove element/s from the list, it couldn't release the block of memory that was allocated. Unless you have a very low amount of memory, this is a micro-optimization. Normally, there is no need to change the capacity of a List, but we can using a method TrimExcess to release a places after remove element.
+         * 6.An array is a method of organizing data in a memory device. A list is a data structure that supports several operations. An array is a collection of homogenous parts, while a list consists of heterogeneous elements. Array has a static elements but List has a felexible elements.
+         */
+        List<int> num2 = new List<int>(); // This is a list with zero element then capacity and count is 0
+        List<int> num3 = new List<int>(1) { 1, 2 }; // This is a list with two element then capacity and count is 2
+        Console.WriteLine("List Size Num2: " + num2.Count() + " and the capacity of the list is: " + num2.Capacity);
+        Console.WriteLine("List Size Num3: " + num3.Count() + " and the capacity of the list is: " + num3.Capacity);
+        num2.Add(525); // Add new element to the num2 list and now the capacity is 4 and count is 2 
+        num3.Add(52);  // Add new element to the num3 list and now the capacity is 4 and count is 3
+        num3.Add(3);   // Add new element to the num3 list and now the capacity is 4 and count is 4
+        num3.Add(5);   // Add new element to the num3 list and now the capacity is 8 and count is 5
+        num3.Add(6);   // Add new element to the num3 list and now the capacity is 8 and count is 6
+        num3.Add(7);   // Add new element to the num3 list and now the capacity is 8 and count is 7
+        num3.Add(8);   // Add new element to the num3 list and now the capacity is 8 and count is 8
+        num3.Add(9);   // Add new element to the num3 list and now the capacity is 16 and count is 9
+        Console.WriteLine("List Size Num2 after add method: " + num2.Count() + " and the capacity of the list is: " + num2.Capacity);
+        Console.WriteLine("List Size Num3 after add method: " + num3.Count() + " and the capacity of the list is: " + num3.Capacity);
+        for(int i = 0; i < 32; i++) { num3.Add(i); num2.Add(i + 30); }
+        // Add new 32 element to the num2 list and now the capacity is 64 and count is 33
+        // Add new 32 element to the num3 list and now the capacity is 64 and count is 41
+        Console.WriteLine("List Size Num2 after add method: " + num2.Count() + " and the capacity of the list is: " + num2.Capacity);
+        Console.WriteLine("List Size Num3 after add method: " + num3.Count() + " and the capacity of the list is: " + num3.Capacity);
+        num3.Remove(9);// Remove 1 element from the num3 list and now the capacity is 64 and count is 40
+        Console.WriteLine("List Size Num3 after add method: " + num3.Count() + " and the capacity of the list is: " + num3.Capacity);
+        for (var i = 0; i < 25; i++) { num3.Remove(i); }
+        // Remove 24 element from the num3 list and now the capacity is 64 and count is 15
+        Console.WriteLine("List Size Num3 after add method: " + num3.Count() + " and the capacity of the list is: " + num3.Capacity);
+        num3.TrimExcess(); // To release a free capacity after remove elemet and now the capacity is 15 and count is 15
+        Console.WriteLine("List Size Num3 after add method: " + num3.Count() + " and the capacity of the list is: " + num3.Capacity);
+
+        /*
+         * Exercise2: ExamineQueue()
+         * 
+         */
     }
-    
+
 }
